912fcc66278359576b893ac57c041a43fd0c97fc
Chanho Park <chanho412@naver.com>
2022-12-05 17:43:46 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 9.39 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        
+        if(s.length()%2 == 0){//짝수인 경우
+            answer += s.charAt(s.length()/2-1);
+            answer += s.charAt(s.length()/2);
+        }else{//홀수인 경우
+            answer += s.charAt(s.length()/2);
+        }
+        return answer;
+    }
+}
