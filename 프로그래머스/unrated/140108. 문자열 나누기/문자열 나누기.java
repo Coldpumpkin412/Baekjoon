6b8a04f2b10df9446fa78992a4bb2f640d9c5465
Chanho Park <chanho412@naver.com>
2022-12-28 17:23:04 +0900

    [unrated] Title: 문자열 나누기, Time: 0.92 ms, Memory: 80.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String s) {
+		int answer = 0;
+		char ch = s.charAt(0); //초기값은 미리 변수에 담아두기
+		int maincount = 1; //기준이 될 알파벳이 나온 횟수(초기값이 이미 나왔으므로 1부터 시작)
+		int subcount = 0; //maincount와 비교할 변수
+		
+		for(int i=1 ; i<s.length() ; i++) {
+			if(ch == ' ')  ch = s.charAt(i);  // 이전에 잘려나가 빈칸인경우 다시 초기값 선정
+			if(ch == s.charAt(i)) maincount++;
+            else subcount++;
+            
+			if(maincount == subcount) { //횟수가 같아지면 다시 초기화
+				answer++;
+				ch = ' ';
+				maincount = 1;
+				subcount = 1;
+			}
+		}
+		
+		if(ch != ' ') answer++;
+		
+		return answer;
+    }
+}
