ef57520fcd60a084c16fd023ef7ba64b356f053c
Chanho Park <chanho412@naver.com>
2023-01-25 15:52:46 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 9.38 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        String[] str = s.split(" ");
+		int[] arr = new int[str.length];
+
+		for(int i=0 ; i<str.length ; i++) arr[i] = Integer.parseInt(str[i]);
+		
+		Arrays.sort(arr);
+		
+        return Integer.toString(arr[0])+" "+Integer.toString(arr[arr.length-1]);
+    }
+}
