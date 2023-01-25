6791fd46ccf4b378d2433cde31ed6ecd07c41681
Chanho Park <chanho412@naver.com>
2023-01-25 15:57:28 +0900

    [level 2] Title: 기능개발, Time: 2.22 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+    	
+    	ArrayList<Integer> answer = new ArrayList<>();
+    	
+    	for(int i=0 ; i< progresses.length ; i++) {
+        	//1.한 개 기능을 개발하는데 필요한 날짜 계산
+    		double days = (100 - progresses[i])/(double)speeds[i];
+    		int daysUp = (int)Math.ceil(days); //실수형을 올림한  후 정수형으로 캐스팅
+    		
+        	//2.함께 배포할 기능의 index 찾기
+    		int j = i+1;
+    		for(; j<progresses.length ; j++) {
+    			if(progresses[j] + daysUp * speeds[j] < 100) break;
+    		}
+        	//3.이번에 배포할 기능의 개수 추가
+    		answer.add(j-i);
+    		i = j-1;
+    	}
+
+
+        //stream은 배열 혹은 컬렉션등의 저장요소를 하나씩 참조하는 동작을 해줌
+        //즉 answer라는 ArrayList의 각각의 Integer객체를 꺼내온 후 mapToInt로 int형태로 바꿔줌
+        return answer.stream().mapToInt(i -> i.intValue()).toArray();
+    }
+}
