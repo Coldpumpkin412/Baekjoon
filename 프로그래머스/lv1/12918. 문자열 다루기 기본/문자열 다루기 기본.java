69ad763c72e6dfd7f2fa4c14f0758d15add9cabd
Chanho Park <chanho412@naver.com>
2023-01-28 16:04:27 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.03 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean solution(String s) {
+        boolean answer = true;
+        
+        if(s.length()==4 || s.length()==6){
+            for(int i=0 ; i<s.length() ; i++){
+                if(0<=s.charAt(i)-'0' && s.charAt(i)-'0'<=9) continue;
+                else return false;
+            }
+        } else return false;
+        
+        return answer;
+    }
+}
