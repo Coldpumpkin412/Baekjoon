379917a212926ed7e34671856dc5156ee1a9ea6e
Chanho Park <chanho412@naver.com>
2023-01-29 14:28:27 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.03 ms, Memory: 65.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length];
+        
+        for(int i=0 ; i<num_list.length ; i++){
+            int tmp = answer[i];
+            answer[i] = num_list[num_list.length-1-i];
+            num_list[num_list.length-1-i] = tmp;
+        }
+        return answer;
+    }
+}
