1c2bd20bbf10585f84ed3f922252f5da30e21b29
Chanho Park <chanho412@naver.com>
2023-01-31 16:07:47 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.08 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(int n, int m) {
+        int[] answer = new int[2];
+        int max=0;//최대공약수
+        
+        for(int i=1 ; i<=n && i<=m ; i++){
+            if(n%i==0 && m%i==0) max = i;
+        }
+        answer[0] = max;
+        
+        answer[1] = n*m/max;
+ 
+        return answer;
+    }
+}
