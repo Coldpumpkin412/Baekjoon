aa9cbbff7bba6d8829a8e59f7324a111ae3d55b5
Chanho Park <chanho412@naver.com>
2023-02-07 16:31:04 +0900

    [unrated] Title: 기사단원의 무기, Time: 0.89 ms, Memory: 87.2 MB -BaekjoonHub

--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.java
@@ -2,11 +2,16 @@ import java.util.*;
 
 class Solution {
     //약수의 개수를 정하는 함수
+    //제곱근이 정수면 약수의 개수는 홀수개
+    //제곱근이 정수가 아니면 약수의 개수는 짝수개
     public static int count(int a){
         int cnt=0;
         
-        for(int i=1 ; i<=a ; i++){
-            if(a%i==0) cnt++;
+        for(int i=1 ; i<=Math.sqrt(a) ; i++){
+            if(a%i==0) {
+                if(a/i==i) cnt++; //제곱근이 정수라면 하나만 올려줘야함
+                else cnt += 2; //가운데 숫자까지만 구하고 두배를 해줘야하기때문
+            }
         }
         
         return cnt;
