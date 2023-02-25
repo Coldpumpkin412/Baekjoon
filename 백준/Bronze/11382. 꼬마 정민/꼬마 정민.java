03905ac0b63784d3b13307d547e23f1113c30f95
Chanho Park <chanho412@naver.com>
2023-02-25 12:07:21 +0900

    [Bronze V] Title: 꼬마 정민, Time: 208 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        
+        //변수 범위 확인 필수!
+		long A = sc.nextLong();
+		long B = sc.nextLong();
+		long C = sc.nextLong();
+		
+		System.out.println(A+B+C);
+		
+	}
+}
+
+	
