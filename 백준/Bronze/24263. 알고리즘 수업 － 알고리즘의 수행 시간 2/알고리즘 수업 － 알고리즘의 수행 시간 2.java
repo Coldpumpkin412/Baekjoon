20483bbd9d351c0afa35ba6922c50e3f8c010622
Chanho Park <chanho412@naver.com>
2023-03-15 16:43:24 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 204 ms, Memory: 17644 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+        /*
+         * for문(반복문)이 1부터 n까지 반복적으로 돌기 때문에
+         * n만큼의 시간복잡도가 나온다
+         */
+		Scanner sc = new Scanner(System.in);
+        
+		int n = sc.nextInt();
+        
+		System.out.println(n);
+		System.out.println('1');
+	}
+}
