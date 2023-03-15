b15472a76055a52249495e86b2b666248a8680ad
Chanho Park <chanho412@naver.com>
2023-03-15 16:41:21 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 120 ms, Memory: 14132 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+        /*
+         * 단일연산이기 때문에 연산횟수에 따라 시간복잡도가 고정으로 나온다
+         */
+		System.out.println('1');
+		System.out.println('0');
+	}
+}
