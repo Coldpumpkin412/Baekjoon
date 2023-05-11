b15c6c05b98034b843d9c9e51ecb0b3652304ee6
Chanho Park <chanho412@naver.com>
2023-05-11 19:09:56 +0900

    [unrated] Title: 주사위 게임 2, Time: 0.03 ms, Memory: 70.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int a, int b, int c) {
+        
+        if(a!=b && b!=c && a!=c) {
+            return a+b+c;
+        } else if(a==b && b==c) {
+            return (int)((a+b+c) * (Math.pow(a,2)+Math.pow(b,2)+Math.pow(c,2)) * (Math.pow(a,3)+Math.pow(b,3)+Math.pow(c,3)));
+        } else if((a==b&&a!=c) || (a==c)&&(a!=b) || (b==c)&&(a!=b)){
+            return (int)((a+b+c) * (Math.pow(a,2)+Math.pow(b,2)+Math.pow(c,2)));
+        }
+        
+        return 1;
+    }
+}
