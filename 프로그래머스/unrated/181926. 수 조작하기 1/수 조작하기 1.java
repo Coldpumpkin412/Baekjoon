9f40586a86dc796c37b47d3d4001b45edbe8c357
Chanho Park <chanho412@naver.com>
2023-05-11 19:28:09 +0900

    [unrated] Title: 수 조작하기 1, Time: 1.12 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n, String control) {
+        
+        for(int i=0 ; i<control.length() ; i++){
+            char ch = control.charAt(i);
+            
+            if(ch == 'w') n++;
+            else if(ch == 's') n--;
+            else if(ch == 'd') n += 10;
+            else n -= 10;
+                
+        }
+        
+        return n;
+    }
+}
