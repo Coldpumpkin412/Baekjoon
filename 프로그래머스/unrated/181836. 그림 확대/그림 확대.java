6154c5fbe5cb21be91c6a7b05dcb2983a73950eb
Chanho Park <chanho412@naver.com>
2023-05-29 19:10:08 +0900

    [unrated] Title: 그림 확대, Time: 13.07 ms, Memory: 88.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181836. 그림 확대/그림 확대.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String[] solution(String[] picture, int k) {
+        String[] answer = new String[picture.length*k];
+		
+		int index=0;
+		for(int i=0 ; i<picture.length ; i++) {
+			String str = "";
+			
+			for(int j=0 ; j<picture[i].length() ; j++) {
+				for(int p=0 ; p<k ; p++) {
+					str += picture[i].charAt(j);
+				}
+			}
+			
+			for(int p=0 ; p<k ; p++) {
+				answer[index] = str;
+				index++;
+			}
+		}
+        
+		return answer;
+    }
+}
