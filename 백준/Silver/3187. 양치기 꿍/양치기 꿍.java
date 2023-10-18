79b9cdc0516a76539879aaf66cf0c0ee148a63a6
Chanho Park <chanho412@naver.com>
2023-10-18 17:55:32 +0900

    [Silver I] Title: 양치기 꿍, Time: 208 ms, Memory: 19536 KB -BaekjoonHub

---
+++ 백준/Silver/3187. 양치기 꿍/양치기 꿍.java
@@ -0,0 +1,106 @@
+import java.util.*;
+import java.io.*;
+
+class Node{
+	int x;
+	int y;
+	
+	Node(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
+
+public class Main {
+	static int R, C;
+	static char[][] map;
+	static boolean[][] visited;
+	
+	//양, 늑대 누적 개수
+	static int sum_sheep;
+	static int sum_wolf;
+	
+	//방향변수
+	static int[] dx = {-1,1,0,0};
+	static int[] dy = {0,0,-1,1};
+	
+	//BFS함수
+	public static void BFS(int row, int col) {
+		//큐 선언
+		Queue<Node> queue = new LinkedList<>();
+		
+		int sheep = 0;
+		int wolf = 0;
+		
+		//초기값 입력 및 방문처리
+		queue.offer(new Node(row, col));
+		visited[row][col] = true;
+		
+		//큐가 빌 때까지
+		while(!queue.isEmpty()) {
+			//현재 노드
+			Node now = queue.poll();
+			
+			
+			if(map[now.x][now.y]=='k') {
+				sheep++;
+			}else if(map[now.x][now.y]=='v') {
+				wolf++;
+			}
+			
+			for(int i=0 ; i<4 ; i++) {
+				int next_row = now.x + dx[i];
+				int next_col = now.y + dy[i];
+				
+				//맵을 벗어나거나, 울타리에 부딪히거나, 방문한적이 있는경우
+				if(next_row<0 || R<=next_row || next_col<0 || C<=next_col || 
+				   map[next_row][next_col]=='#' || visited[next_row][next_col]) {
+					continue;
+				}
+				
+				//방문처리 및 큐에 넣어주기
+				visited[next_row][next_col] = true;
+				queue.offer(new Node(next_row, next_col));
+			}
+		}
+		
+		//양의 수가 더 많은경우
+		if(sheep > wolf) {
+			sum_sheep += sheep;
+		}else {
+			sum_wolf += wolf;
+		}
+		
+	}
+	
+    public static void main(String[] args) throws IOException {
+    	//BFS활용하여 풀기
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st;
+    	
+    	//마당의 행 R, 열 C 입력
+    	st = new StringTokenizer(br.readLine());
+    	R = Integer.parseInt(st.nextToken());
+    	C = Integer.parseInt(st.nextToken());
+    	
+    	//마당 배열 및 방문처리 배열 선언
+    	map = new char[R][C];
+    	visited = new boolean[R][C];
+    	
+    	//마당 입력
+    	for(int i=0 ; i<R ; i++) {
+    		map[i] = br.readLine().toCharArray();
+    	}
+    	
+    	for(int i=0 ; i<R ; i++) {
+    		for(int j=0 ; j<C ; j++) {
+    			//방문한적이 없는경우 BFS함수
+    			if(!visited[i][j] && map[i][j]!='#') {
+    				BFS(i, j);
+    			}
+    		}
+    	}
+    	
+    	System.out.println(sum_sheep + " " + sum_wolf);
+    }
+}	
