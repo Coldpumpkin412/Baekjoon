8f0505f2bf419d2105e2e10e4a9b102348665275
Chanho Park <chanho412@naver.com>
2023-10-30 19:17:11 +0900

    [Bronze I] Title: 나머지와 몫이 같은 수, Time: 212 ms, Memory: 17596 KB -BaekjoonHub

---
+++ 백준/Bronze/1834. 나머지와 몫이 같은 수/나머지와 몫이 같은 수.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        
+		long num = sc.nextLong();
+		long sum = 0;
+
+		for (int i=0 ; i<num ; i++) {
+			sum += (num+1) * i;
+		}
+        
+		System.out.println(sum);
+	}
+}
