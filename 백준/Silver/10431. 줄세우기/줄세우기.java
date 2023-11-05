871583eb7977cea728c4e1c5c803c7b4113f2298
Chanho Park <chanho412@naver.com>
2023-11-05 13:03:50 +0900

    [Silver V] Title: 줄세우기, Time: 236 ms, Memory: 17876 KB -BaekjoonHub

---
+++ 백준/Silver/10431. 줄세우기/줄세우기.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new java.io.InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+        
+		int T = Integer.parseInt(br.readLine());
+		for(int t=1 ; t<=T ; t++) {
+			sb.append(t+" ");
+            
+			List<Integer> height = new ArrayList<>();
+			String[] split = br.readLine().split(" ");
+			
+			int where, cnt = 0;
+			height.add(Integer.parseInt(split[1]));
+			
+			for(int i=2 ; i<=20 ; i++) {
+				int now = Integer.parseInt(split[i]);
+				
+				where = -1;
+                
+				for(int j=0 ; j<i-1 ; j++) {
+					if(height.get(j)>now) {
+						where = j;
+						break;
+					}
+				}
+                
+				if(where == -1) { //다 작았을 때
+					height.add(now);
+				} else {
+					height.add(where, now);
+					cnt += height.size()-where-1;
+				}
+                
+			}
+            
+			sb.append(cnt).append("\n");
+		}
+        
+		System.out.println(sb);
+	}	
+}
