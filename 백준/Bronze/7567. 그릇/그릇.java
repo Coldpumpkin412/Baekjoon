929a2ab3288783193e40ffa969cec94c6c41c1bf
Chanho Park <chanho412@naver.com>
2024-04-06 18:22:52 +0900

    [Bronze II] Title: 그릇, Time: 124 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/7567. 그릇/그릇.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String string = br.readLine();
+
+        int answer = 10;
+
+        for (int i=1 ; i<string.length() ; i++){
+            if (string.charAt(i) == string.charAt(i - 1)){
+                answer += 5;
+            }
+            else{
+                answer += 10;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
