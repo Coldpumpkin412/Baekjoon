65231087283cef48fd14cefe405ca162cfb357a8
Chanho Park <chanho412@naver.com>
2024-04-09 17:43:41 +0900

    [Silver I] Title: 알고리즘 수업 - 퀵 정렬 3, Time: 380 ms, Memory: 21940 KB -BaekjoonHub

--- 백준/Silver/24092. 알고리즘 수업 － 퀵 정렬 3/알고리즘 수업 － 퀵 정렬 3.java
+++ 백준/Silver/24092. 알고리즘 수업 － 퀵 정렬 3/알고리즘 수업 － 퀵 정렬 3.java
@@ -2,93 +2,108 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-
-    static int[] B;
-    static int check;
-    
-    //퀵소트
-    public static void quick_sort(int[] A, int left, int right){
-
-        if(left >= right) return;
-        if(check == 1) return;
-
-        int part2 = partition(A, left, right);
-
-        quick_sort(A, left, part2-1);
-        quick_sort(A, part2+1, right);
-    }
-    
-    //파티션 함수
-    public static int partition(int[] A, int left, int right){
-        int lo = left;
-        int hi = right;
+	static int[] B;
+	static boolean check;
+	
+	//퀵소트
+	static void quick_sort(int[] arr, int start, int end) {
+		//시작포인터가 끝포인터를 넘거나 check변수가 true일 때
+		if(start>=end || check) {
+			return;
+		}
+		
+		//분할값
+		int part2 = partition(arr, start, end);
+		
+		//왼쪽부분 정렬
+		quick_sort(arr, start, part2-1);			
+		
+		//오른쪽부분 정렬
+		quick_sort(arr, part2+1, end);
+	}
+	
+	//파티션
+	static int partition(int[] arr, int start, int end) {
+		
+		//피벗값(맨 끝값)
+		int pivot = arr[end];
+		
+		//각각 시작점, 끝점 포인터
+		int left = start;
+		int right = end;
+		
+		//겹치지 않을때까지
+		while(left < right) {
+			//피벗값보다 큰 값을 찾을때까지 시작점 인덱스 이동
+			while(left<right && arr[left]<pivot) {
+				left++;
+			}
+			
+			//피벗값보다 작은 값을 찾을때까지 끝점 인덱스 이동
+			while(left<right && arr[right]>=pivot) {
+				right--;
+			}
+			
+			//교환
+			swap(arr, left, right);
+			
+			//배열 비교
+			compare(arr);
+		}
+		
+		//교환 및 비교
+		swap(arr, right, end);
+		compare(arr);
+		
+		return right;
+	}
+	
+	//교환함수
+	static void swap(int[] arr, int num1, int num2) {
+		int tmp = arr[num1];
+		arr[num1] = arr[num2];
+		arr[num2] = tmp;		
+	}
+	
+	//배열 비교함수
+	static boolean compare(int[] arr) {
+		//비교배열과 다르다면
+		if(!Arrays.equals(arr, B)) {
+			return false;
+		}else { //같다면
+			check = true;
+			return true;
+		}
+	}
+	
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st;
+    	
+    	//배열 A, B의 크기 N 입력
+    	int N = Integer.parseInt(br.readLine());
+    	
+    	//배열 A, B 선언 및 값 입력
+    	int[] A = new int[N];
+    	B = new int[N];
+    	
+    	st = new StringTokenizer(br.readLine());
+    	for(int i=0 ; i<N ; i++) {
+    		A[i] = Integer.parseInt(st.nextToken());
+    	}
+    	
+    	st = new StringTokenizer(br.readLine());
+    	for(int i=0 ; i<N ; i++) {
+    		B[i] = Integer.parseInt(st.nextToken());
+    	}
         
-        //피벗값
-        int pivot = A[right];
-
-        while(lo < hi){
-            //피벗값보다 큰 값을 찾을때까지 시작점 인덱스 이동
-            while(lo<hi && A[lo]<pivot){
-                lo++;
-            }
-            //피벗값보다 작은 값을 찾을때까지 끝점 인덱스 이동
-            while(lo < hi && A[hi]>=pivot){
-                hi--;
-            }
-            
-            //교환 후 비교
-            swap(A, lo, hi);
-            compare(A); 
-        }
-        
-        //교환 후 비교
-        swap(A, hi, right);
-        compare(A);
-        return hi;
+    	//정렬 전 두 배열 비교
+    	compare(A);
+    	
+    	//A배열 퀵소트
+    	quick_sort(A, 0, N-1);
+    	
+    	//체크변수가 true면 1을, 아니면 0을 반환
+    	System.out.println(check ? 1 : 0);
     }
-    
-    //교환함수
-    public static void swap(int[] A, int i, int j){
-        int temp = A[i];
-        A[i] = A[j];
-        A[j] = temp;
-    }
-    
-    //비교함수
-    public static boolean compare(int[] A){
-        for(int i=0 ; i<A.length ; i++){
-            if(A[i] != B[i]){
-                return false;
-            }
-        }
-        
-        check = 1;
-        return true;
-    }
-    
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        
-        int N = Integer.parseInt(br.readLine());
-        int[] A = new int[N];
-        B = new int[N];
-        
-        check = 0;
-
-        st = new StringTokenizer(br.readLine());
-        for(int i=0 ; i<N ; i++){
-            A[i] = Integer.parseInt(st.nextToken());
-        }
-
-        st = new StringTokenizer(br.readLine());
-        for(int i=0 ; i<N ; i++){
-            B[i] = Integer.parseInt(st.nextToken());
-        }
-
-        compare(A);
-        quick_sort(A, 0, N - 1);
-
-        System.out.println(check);
-    }
-}
+}	
