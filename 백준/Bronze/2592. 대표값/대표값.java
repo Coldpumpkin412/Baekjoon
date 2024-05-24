06c7faf76bf1d89fe1fd5250238913e39dbb345c
Chanho Park <chanho412@naver.com>
2024-05-24 09:19:01 +0900

    [Bronze II] Title: 대표값, Time: 228 ms, Memory: 18400 KB -BaekjoonHub

---
+++ 백준/Bronze/2592. 대표값/대표값.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        
+        int sum = 0;
+        int[] arr = new int [1001];
+        
+        for(int i=0 ; i<10 ; i++){
+            int number = sc.nextInt();
+            sum += number;
+            arr[number]++;            
+        }
+
+        int index = 0;
+        int max = 0;
+        
+        for(int i=0 ; i<1001 ; i++){
+            if(max < arr[i]){
+                max = arr[i];
+                index = i;
+            }
+        }
+
+        System.out.print((sum/10) + "\n" + index);
+
+    }
+}
