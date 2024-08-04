6000ecefe597233e30406d5ac8262b42204b7315
Chanho Park <chanho412@naver.com>
2024-08-04 17:52:09 +0900

    [Bronze II] Title: 8진수, 10진수, 16진수, Time: 104 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/11816. 8진수， 10진수， 16진수/8진수， 10진수， 16진수.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+		String num = br.readLine();
+		
+		if(num.charAt(0) != '0') {
+			System.out.print(num);
+		}else if(num.charAt(1) != 'x') {
+			System.out.print(Integer.valueOf(Integer.parseInt(num.substring(1), 8)));
+		}else {
+			System.out.print(Integer.valueOf(Integer.parseInt(num.substring(2), 16)));
+		}
+	}
+
+}
