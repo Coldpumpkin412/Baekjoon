4ca435692f1e03194e77a4736fc4e3a744d4c7a9
Chanho Park <chanho412@naver.com>
2024-10-02 22:52:19 +0900

    [Bronze II] Title: 진짜 공간, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/1350. 진짜 공간/진짜 공간.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+		int N = Integer.parseInt(br.readLine());
+		long count = 0;
+		
+		st = new StringTokenizer(br.readLine());
+		long size = Long.parseLong(br.readLine());
+		for(int i=0 ; i<N ; i++) {
+			long file = Long.parseLong(st.nextToken());
+			
+			//파일 크기가 0이 아닐때만 연산
+			if(file != 0) {
+				count += (file / size);
+				if(file%size != 0) {
+					count++;
+				}
+			}
+		}
+		System.out.println(size * count);
+	}
+
+}
