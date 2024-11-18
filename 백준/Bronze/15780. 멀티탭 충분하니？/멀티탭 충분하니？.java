0efedef1a9ad7586df5ac0fdbcc3d505e810486f
Chanho Park <chanho412@naver.com>
2024-11-18 22:23:14 +0900

    [Bronze III] Title: 멀티탭 충분하니?, Time: 112 ms, Memory: 14312 KB -BaekjoonHub

---
+++ 백준/Bronze/15780. 멀티탭 충분하니？/멀티탭 충분하니？.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        //학생 수 N, 멀티탭 수 K 입력
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        
+        //멀티탭 별로 구의 수 저장할 배열 선언 및 값 입력
+        int[] A = new int[K];
+        
+        st = new StringTokenizer(br.readLine());
+        for(int i=0 ; i<K ; i++){
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        //총 꽂을 수 있는 수
+        int count = 0;
+        
+        for(int i=0 ; i<K ; i++){
+            //구의 수가 짝수일 경우
+            if(A[i]%2 == 0){
+                count += A[i]/2;
+            }else{
+                count += A[i]/2+1;
+            }
+        }
+        
+        
+        System.out.println((N<=count) ? "YES" : "NO");
+                    
+    }
+}
