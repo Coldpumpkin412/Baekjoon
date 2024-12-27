5b63deac75e9b306e6a78cf2fa8c4803ae91b5d7
Chanho Park <chanho412@naver.com>
2024-12-27 16:25:49 +0900

    [Bronze V] Title: Multiply, Time: 592 ms, Memory: 55728 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/22193. Multiply/Multiply.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		br.readLine();
+		
+		System.out.print(new BigInteger(br.readLine()).multiply(new BigInteger(br.readLine())));
+	}
+}
