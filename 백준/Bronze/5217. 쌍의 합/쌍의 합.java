1ad213ecd6d598e09408407dc3c466f5988e1132
Chanho Park <chanho412@naver.com>
2025-01-07 18:10:34 +0900

    [Bronze III] Title: 쌍의 합, Time: 144 ms, Memory: 16432 KB -BaekjoonHub

---
+++ 백준/Bronze/5217. 쌍의 합/쌍의 합.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i < T; i++) {
+			int n = Integer.parseInt(br.readLine());
+            
+			String S = "Pairs for " + n + ": ";
+			
+			for(int j=1 ; j<=n/2 ; j++) {
+				
+				if(j != (n - j)) {
+					
+					if(j > 1) {
+						S += ", ";
+					}
+                    
+					S += j + " " + (n - j);
+				}
+			}
+			//출력
+			System.out.println(S);
+		}
+	}
+
+}
