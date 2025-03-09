a7fb5181b442f9ef5220f7269f2e8b2bef6b5247
Chanho Park <chanho412@naver.com>
2025-03-09 15:55:53 +0900

    [Bronze II] Title: 수 뒤집기, Time: 128 ms, Memory: 16056 KB -BaekjoonHub

---
+++ 백준/Bronze/3062. 수 뒤집기/수 뒤집기.java
@@ -0,0 +1,38 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i=0 ; i<N ; i ++){
+            String cur = br.readLine();
+            String cur2 = "";
+
+            for(int j=cur.length()-1 ; j>=0 ; j --){
+                cur2 += cur.charAt(j);
+            }
+                
+
+            String res = String.valueOf(Integer.parseInt(cur) + Integer.parseInt(cur2));
+
+            int l = 0;
+            int r = res.length()-1;
+            
+            boolean check = true;
+
+            while(r > l){
+                if(res.charAt(l++) != res.charAt(r--)){
+                    check = false;
+                    break;
+                }
+            }
+
+            sb.append(check ? "YES" : "NO").append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
