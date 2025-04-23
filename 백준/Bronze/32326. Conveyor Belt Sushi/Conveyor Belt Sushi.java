e8d4c8b3ab6cd1e56a68068e6690079e52a0fe5b
Chanho Park <chanho412@naver.com>
2025-04-23 21:14:35 +0900

    [Bronze V] Title: Conveyor Belt Sushi, Time: 168 ms, Memory: 17528 KB -BaekjoonHub

---
+++ 백준/Bronze/32326. Conveyor Belt Sushi/Conveyor Belt Sushi.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int R = sc.nextInt();
+		int G = sc.nextInt();
+		int B = sc.nextInt();
+        
+		System.out.println(3*R + 4*G + 5*B);
+	}
+}
