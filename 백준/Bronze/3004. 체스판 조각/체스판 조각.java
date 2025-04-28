9a6ea287747c39d029d230ac3bd4b7f575c2eb5a
Chanho Park <chanho412@naver.com>
2025-04-28 17:59:14 +0900

    [Bronze III] Title: 체스판 조각, Time: 100 ms, Memory: 14092 KB -BaekjoonHub

---
+++ 백준/Bronze/3004. 체스판 조각/체스판 조각.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+		int N = Integer.parseInt(br.readLine());
+        
+		int sum = 0;
+		
+		for(int i=1 ; i<=N ; i++) {
+			if(i == 1) {
+				sum += 2;
+			}else {
+				sum += ((i/2) + 1);
+			}
+		}
+        
+        System.out.println(sum);
+	}
+}
