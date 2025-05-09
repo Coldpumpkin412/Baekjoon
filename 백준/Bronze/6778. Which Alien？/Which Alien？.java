8c1f9364cdc3b36963593f6a066840c764cb5326
Chanho Park <chanho412@naver.com>
2025-05-09 22:32:41 +0900

    [Bronze IV] Title: Which Alien?, Time: 168 ms, Memory: 17584 KB -BaekjoonHub

---
+++ 백준/Bronze/6778. Which Alien？/Which Alien？.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int E = sc.nextInt();
+
+
+        if(A>=3 && E<=4){
+            System.out.println("TroyMartian");
+        }if(A<=6 && E>=2 ){
+            System.out.println("VladSaturnian");
+        }if(A<=2 && E<=3){
+            System.out.println("GraemeMercurian");
+        }else {
+
+        }
+
+    }
+}
+ 
