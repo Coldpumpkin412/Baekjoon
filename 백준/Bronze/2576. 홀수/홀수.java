2342aa63854b8ee20845d233d7c41ec8b908ba67
Chanho Park <chanho412@naver.com>
2025-06-01 23:13:36 +0900

    [Bronze III] Title: 홀수, Time: 96 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/2576. 홀수/홀수.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+		int sum = 0;
+		int min = 100;
+		
+		for(int i=0 ; i<7; i++) {
+			int N = Integer.parseInt(br.readLine());
+			
+			if(N%2 == 1) {
+				sum += N;
+                
+				min = Math.min(min, N);
+			}
+		}
+		
+		if(sum == 0) {
+			System.out.println(-1);
+		}else {
+			System.out.println(sum);
+			System.out.println(min);
+		}
+	}
+
+}
