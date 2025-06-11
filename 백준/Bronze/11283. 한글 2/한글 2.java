788c2a5055af3c1be04822cddbddd1793324b601
Chanho Park <chanho412@naver.com>
2025-06-11 23:24:05 +0900

    [Bronze IV] Title: 한글 2, Time: 172 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/11283. 한글 2/한글 2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        
+		char ch = sc.nextLine().charAt(0);
+
+		System.out.println((int)ch - 44031);
+
+	}
+
+}
