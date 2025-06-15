7ad508e83a53d09b1ab2705ab44f2c757c1db542
Chanho Park <chanho412@naver.com>
2025-06-15 16:07:39 +0900

    [Bronze III] Title: ABC, Time: 208 ms, Memory: 18372 KB -BaekjoonHub

---
+++ 백준/Bronze/3047. ABC/ABC.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        
+		int array[] = new int[3];
+
+		array[0] = sc.nextInt();
+		array[1] = sc.nextInt();
+		array[2] = Integer.parseInt(sc.nextLine().trim());
+
+		String str = sc.nextLine().trim();
+
+		Arrays.sort(array);
+
+		for(int i=0 ; i<3 ; i++) {
+			char c = str.charAt(i);
+
+			if (c == 'A')
+				System.out.print(array[0] + " ");
+			if (c == 'B')
+				System.out.print(array[1] + " ");
+			if (c == 'C')
+				System.out.print(array[2] + " ");
+		}
+
+	}
+}
