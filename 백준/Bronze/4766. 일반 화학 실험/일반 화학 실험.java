4bfd1a06a44282a46c0232683cd7ecab1116a32b
Chanho Park <chanho412@naver.com>
2025-06-30 18:29:44 +0900

    [Bronze III] Title: 일반 화학 실험, Time: 104 ms, Memory: 14376 KB -BaekjoonHub

---
+++ 백준/Bronze/4766. 일반 화학 실험/일반 화학 실험.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+		double N = Double.parseDouble(br.readLine());
+		
+		while(true) {
+			double N2 = Double.parseDouble(br.readLine());
+            
+			if(N2 == 999) {
+				break;
+			}
+			System.out.println(String.format("%.2f", (N2 - N)));
+            
+			N = N2;
+		}
+	}
+
+}
