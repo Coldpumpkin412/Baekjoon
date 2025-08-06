927a2b1f4738228aa3a06bbe7b85f1b10ffb359a
Chanho Park <chanho412@naver.com>
2025-08-06 23:08:54 +0900

    [Bronze V] Title: 크냐?, Time: 108 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+        
+		while(true) {
+			st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+			
+			if(N==0 && M==0) {
+				break;
+			}
+			
+			System.out.println((N>M) ? "Yes" : "No");
+		}
+		
+	}
+
+}
