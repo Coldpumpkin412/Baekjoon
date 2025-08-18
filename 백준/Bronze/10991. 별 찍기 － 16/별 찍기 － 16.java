9e2b69c55b518eea255ef6fafbbd57b1d9749f12
Chanho Park <chanho412@naver.com>
2025-08-18 23:06:38 +0900

    [Bronze III] Title: 별 찍기 - 16, Time: 112 ms, Memory: 14348 KB -BaekjoonHub

---
+++ 백준/Bronze/10991. 별 찍기 － 16/별 찍기 － 16.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i=1 ; i<=N ; i++) {
+            for(int j=N ; j>i ; j--) {
+                sb.append(" ");
+            }
+            for(int j=0 ; j<2*i-1 ; j++) {
+               if(j%2 == 0) {
+                   sb.append("*");
+               }
+               if(j%2 == 1) {
+                   sb.append(" ");
+               }
+            }
+            sb.append("\n");
+        }
+        
+        System.out.println(sb.toString());
+    }
+}
