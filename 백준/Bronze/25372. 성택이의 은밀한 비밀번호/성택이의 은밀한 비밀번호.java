f677b8c49c53285d5cbab979aeb63ca821e6a10a
Chanho Park <chanho412@naver.com>
2025-09-10 22:56:17 +0900

    [Bronze V] Title: 성택이의 은밀한 비밀번호, Time: 108 ms, Memory: 14544 KB -BaekjoonHub

---
+++ 백준/Bronze/25372. 성택이의 은밀한 비밀번호/성택이의 은밀한 비밀번호.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i=0 ; i<N ; i ++){
+            String str = br.readLine();
+
+            if(str.length()>=10 || str.length()<6){
+                sb.append("no");
+                
+            }else{
+                sb.append("yes");
+            }
+            
+            sb.append("\n");
+        }
+        System.out.print(sb.toString());
+    }
+}
