2b41acc8c1e6efdcefe521c36edd550bc582a216
Chanho Park <chanho412@naver.com>
2025-09-14 17:58:40 +0900

    [Bronze IV] Title: Darius님 한타 안 함?, Time: 112 ms, Memory: 14044 KB -BaekjoonHub

---
+++ 백준/Bronze/20499. Darius님 한타 안 함？/Darius님 한타 안 함？.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+		String str = br.readLine();
+        
+		String[] arr = str.split("/");
+        
+		int K = Integer.parseInt(arr[0]);
+		int D = Integer.parseInt(arr[1]);
+		int A = Integer.parseInt(arr[2]);
+		
+		if((K+A)<D || D==0) {
+			System.out.println("hasu");
+		}else {
+			System.out.println("gosu");
+		}
+	}
+
+}
