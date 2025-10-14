2e5ab22fc6b31b87a5f5e698f8075bf07aa37da9
Chanho Park <chanho412@naver.com>
2025-10-14 20:57:08 +0900

    [Bronze III] Title: 폰 노이만, Time: 136 ms, Memory: 16592 KB -BaekjoonHub

---
+++ 백준/Bronze/9469. 폰 노이만/폰 노이만.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+		int T = Integer.parseInt(br.readLine());
+		
+		while(T-->0) {
+			st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			double D = Double.parseDouble(st.nextToken());
+			double A = Double.parseDouble(st.nextToken());
+			double B = Double.parseDouble(st.nextToken());
+			double F = Double.parseDouble(st.nextToken());
+			
+			System.out.println(N + " " + String.format("%.6f", ((D / (A + B)) * F)));
+		}
+	}
+
+}
