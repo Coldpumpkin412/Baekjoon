ffe3a2df620b4ac94acfeb4000a2b6a03330be9d
Chanho Park <chanho412@naver.com>
2025-10-25 11:06:55 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 108 ms, Memory: 15852 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int[] arr = new int[26];
+        
+        String s = br.readLine();
+        
+        for(int i=0 ; i<s.length() ; i++) {
+            char c = s.charAt(i);
+            
+            arr[c - 97]++;  //개수 1씩 늘려주기
+        }
+        
+        for(int i=0 ; i<26 ; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
