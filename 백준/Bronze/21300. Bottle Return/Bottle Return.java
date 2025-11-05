ec4b5375bd7ab64b7e14e9413a0bfec8666290e1
Chanho Park <chanho412@naver.com>
2025-11-05 17:25:45 +0900

    [Bronze V] Title: Bottle Return, Time: 104 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/21300. Bottle Return/Bottle Return.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+		String s = br.readLine();
+        
+		StringTokenizer st = new StringTokenizer(s);
+        
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		int d = Integer.parseInt(st.nextToken());
+		int e = Integer.parseInt(st.nextToken());
+		int f = Integer.parseInt(st.nextToken());
+        
+		int sum = a+b+c+d+e+f;
+        
+		System.out.println(sum * 5);
+
+	}
+
+}
