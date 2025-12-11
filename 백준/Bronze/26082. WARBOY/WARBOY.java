9a20ff1310f394619b0b12102af7ad11a1db416c
Chanho Park <chanho412@naver.com>
2025-12-11 23:03:02 +0900

    [Bronze V] Title: WARBOY, Time: 92 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Bronze/26082. WARBOY/WARBOY.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        
+        System.out.println((B/A*3) * C);
+    }
+}
