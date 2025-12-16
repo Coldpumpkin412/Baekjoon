c46870c56feebbe4b62c01d463a017a95881a57f
Chanho Park <chanho412@naver.com>
2025-12-16 18:17:08 +0900

    [Bronze IV] Title: 운동장 한 바퀴, Time: 184 ms, Memory: 17664 KB -BaekjoonHub

---
+++ 백준/Bronze/16486. 운동장 한 바퀴/운동장 한 바퀴.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int D1 = sc.nextInt();
+        int D2 = sc.nextInt();
+        
+        double PI = 3.141592;
+        
+        System.out.println(2*D1 + 2*D2*PI);
+    }
+}
