9182625592de2199371788b33e57dd2323c2ecc8
Chanho Park <chanho412@naver.com>
2025-12-28 18:09:44 +0900

    [Bronze IV] Title: 폰 노이만과 파리, Time: 172 ms, Memory: 17572 KB -BaekjoonHub

---
+++ 백준/Bronze/14924. 폰 노이만과 파리/폰 노이만과 파리.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int S = sc.nextInt();
+        int T = sc.nextInt();
+        int D = sc.nextInt();
+        
+        System.out.println(D/(2*S)*T);
+    }
+}
