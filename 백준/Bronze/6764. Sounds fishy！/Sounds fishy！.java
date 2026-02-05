d0051d3bd5e1be9befd017797d04f5e5ca7371fa
Chanho Park <chanho412@naver.com>
2026-02-05 22:32:56 +0900

    [Bronze IV] Title: Sounds fishy!, Time: 168 ms, Memory: 17644 KB -BaekjoonHub

---
+++ 백준/Bronze/6764. Sounds fishy！/Sounds fishy！.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int[] arr = new int[4];
+        
+        for(int i=0 ; i<arr.length ; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        if(arr[0]<arr[1] && arr[1]<arr[2] && arr[2]<arr[3]){
+            System.out.printf("Fish Rising");
+        }else if(arr[0]>arr[1] && arr[1]>arr[2] && arr[2]>arr[3]){
+            System.out.printf("Fish Diving");
+        }else if(arr[0]==arr[1] && arr[1]==arr[2] && arr[2]==arr[3]){
+            System.out.printf("Fish At Constant Depth");
+        }else {
+            System.out.printf("No Fish");
+        }
+
+    }
+}
