2f4551e1cf6cda7e8d6e9e60a73ca618bf4b0c63
Chanho Park <chanho412@naver.com>
2026-02-19 22:37:27 +0900

    [Bronze IV] Title: Counting Clauses, Time: 168 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/17903. Counting Clauses/Counting Clauses.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int M = sc.nextInt();
+		int N = sc.nextInt();
+		
+		if(M >= 8) {
+			System.out.println("satisfactory");
+		}else {
+			System.out.println("unsatisfactory");
+		}
+        
+	}
+}
