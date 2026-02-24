5664ade5bf94336bf6248676858c4f2ff5416010
Chanho Park <chanho412@naver.com>
2026-02-24 17:14:08 +0900

    [Bronze IV] Title: Blaster's Dash, Time: 172 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/34380. Blaster's Dash/Blaster's Dash.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int m = sc.nextInt();
+        int v = sc.nextInt();
+        int n = sc.nextInt();
+            
+        int answer = 2 * (20 + n);
+           
+        System.out.println(answer);        
+    }
+}
