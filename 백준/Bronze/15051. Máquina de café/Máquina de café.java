7a7e5af015ba8f6710db7cea1856c39b323b23f1
Chanho Park <chanho412@naver.com>
2026-03-15 21:01:40 +0900

    [Bronze IV] Title: Máquina de café, Time: 100 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/15051. Máquina de café/Máquina de café.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int a1 = Integer.parseInt(br.readLine());
+        int a2 = Integer.parseInt(br.readLine());
+        int a3 = Integer.parseInt(br.readLine());
+
+        int result = Math.min(a2*2 + a3*4, Math.min(a1*2 + a3*2, a1*4 + a2*2));
+
+        System.out.println(result);
+    }
+}
