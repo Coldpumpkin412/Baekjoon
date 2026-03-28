db58e4ec4a93435d320746aa232af4bf231da867
Chanho Park <chanho412@naver.com>
2026-03-28 09:33:52 +0900

    [Bronze IV] Title: 체스 초보 브실이, Time: 176 ms, Memory: 17580 KB -BaekjoonHub

---
+++ 백준/Bronze/29725. 체스 초보 브실이/체스 초보 브실이.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int count = 0;
+
+        for(int i=0 ; i<8 ; i++) {
+            String a = sc.next();
+            
+            for(int j=0 ; j<a.length() ; j++) {
+                
+                if(a.charAt(j) == 'K') {
+                    count += 0;
+                }else if(a.charAt(j) == 'P') {
+                    count += 1;
+                }else if(a.charAt(j) == 'N') {
+                    count += 3;
+                }else if(a.charAt(j) == 'B') {
+                    count += 3;
+                }else if(a.charAt(j) == 'R') {
+                    count += 5;
+                }else if(a.charAt(j) == 'Q') {
+                    count += 9;
+                }else if(a.charAt(j) == 'k') {
+                    count -= 0;
+                }else if(a.charAt(j) == 'p') {
+                    count -= 1;
+                }else if(a.charAt(j) == 'n') {
+                    count -= 3;
+                }else if(a.charAt(j) == 'b') {
+                    count -= 3;
+                }else if(a.charAt(j) == 'r') {
+                    count -= 5;
+                }else if(a.charAt(j) == 'q') {
+                    count -= 9;
+                }
+            }
+        }
+
+        System.out.println(count);
+    }
+}
