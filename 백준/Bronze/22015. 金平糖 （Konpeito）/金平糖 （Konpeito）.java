81d3700e97a4e49fbb09db8aab7c0a74d243ad71
Chanho Park <chanho412@naver.com>
2026-03-30 22:20:20 +0900

    [Bronze IV] Title: 金平糖 (Konpeito), Time: 108 ms, Memory: 14124 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/22015. 金平糖 （Konpeito）/金平糖 （Konpeito）.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        System.out.println(Math.max(Math.max(a, b), c) * 3 - (a+b+c));
+    }
+}
